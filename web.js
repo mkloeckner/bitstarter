4a8b550328721e87e2ba00a926832c0967408654
Martin <m.kloeckner@gmail.com>
2013-07-11 22:17:19 +0000

    some fix

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html');
-  response.send(buffer.toString();
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
