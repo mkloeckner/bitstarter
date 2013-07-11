3169a63389fd01d531e20d732d0bdc2a48f0ca0a
Martin <m.kloeckner@gmail.com>
2013-07-11 22:04:12 +0000

    some fix

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFile('index.html'));
+  var buffer = new Buffer(16);
+  buffer.write(fs.readFile('index.html'));
 
   response.send(buffer.toString());
 });
